1d6553f1adf687f769d4b26a7d6ce1647a97594b
Max <max.bovin@elev.ga.lbs.se>
2026-04-25 14:44:48 +0200

    Fix 2
    i hate canvas

--- Untitled_Island_GameDigDig02/Assets/Game assets/Scripts/General Scripts/EndingScript.cs
+++ Untitled_Island_GameDigDig02/Assets/Game assets/Scripts/General Scripts/EndingScript.cs
@@ -8,21 +8,21 @@ public class EndingScript : MonoBehaviour
     
     public GameObject ShipRepair;
     public GameObject Text;
-    private void OnTriggerStay(Collider other)
+   
+
+    private void Update()
     {
 
         if (QuestManager.Questscomplteted >= 5f)
         {
-            
+
             ShipRepair.SetActive(true);
             Text.SetActive(true);
-          // ViewBlock.SetActive(true);
-          // SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
-            
+            // ViewBlock.SetActive(true);
+            // SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
+
 
         }
 
     }
-
-
 }
